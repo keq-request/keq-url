1c1ebfe7e93550003c71a106c4beb8fe51f04cb1
val.istar.guo <val.istar.guo@gmail.com>
2024-08-09 17:30:02 +0800

    fix: port is set to :0 in chrome@70

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,14 @@ export function setBaseUrl(base: string): KeqMiddleware {
   return async function setBaseUrl(ctx, next) {
     ctx.request.url.host = url.host
     ctx.request.url.protocol = url.protocol
-    ctx.request.url.port = url.port
     ctx.request.url.pathname = `${url.pathname.replace(/\/$/, '')}/${ctx.request.url.pathname.replace(/^\//, '')}`
+
+    // Fix bug in chrome@74 bug:
+    //   url.port will return `""` if the base url not contain port.
+    //   And when ctx.response.url.port is set to `""`, `null` or `undefined`, it will return `:0` in ctx.response.url.href
+    if (url.port) {
+      ctx.request.url.port = url.port
+    }
 
     await next()
   }
@@ -20,7 +26,13 @@ export function setOrigin(origin: string): KeqMiddleware {
   return async function setOrigin(ctx, next) {
     ctx.request.url.host = url.host
     ctx.request.url.protocol = url.protocol
-    ctx.request.url.port = url.port
+
+    // Fix bug in chrome@74 bug:
+    //   url.port will return `""` if the base url not contain port.
+    //   And when ctx.response.url.port is set to `""`, `null` or `undefined`, it will return `:0` in ctx.response.url.href
+    if (url.port) {
+      ctx.request.url.port = url.port
+    }
 
     await next()
   }
